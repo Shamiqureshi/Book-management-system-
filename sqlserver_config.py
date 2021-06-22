374626396f4b9c1ed07a88d6fa2473131b30e9ee
SHAMI QURESHI <61318354+Shamiqureshi@users.noreply.github.com>
2021-06-22 13:14:38 +0530

    initial upload

---
+++ sqlserver_config.py
@@ -0,0 +1,7 @@
+dbConfig = {
+    'driver':'SQL Server',
+    'Server': "SHAMIQURESHI\SQLEXPRESS",
+    'Database': "mybooks",
+    'username' :"SHAMIQURESHI\admin",
+    "password": "mypassword"
+}
